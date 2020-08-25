2afcac2f0702688ebdecdd99f97fcad42aacd974
Donatas Vasauskas <dvasausk@wvup.edu>
2020-08-24 22:04:05 -0400

    Trying tests

---
+++ BlackJack/BlackjackTests1/DealerTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Blackjack;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blackjack.Tests
+{
+    [TestClass()]
+    public class DealerTests
+    {
+        [TestMethod()]
+        public void DealTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DealerTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ BlackJack/BlackjackTests1/PlayerTests.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Blackjack;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blackjack.Tests
+{
+    [TestClass()]
+    public class PlayerTests
+    {
+        [TestMethod()]
+        public void PlayerTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void PlayerTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ReceiveCardTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ClearHandTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ScoreHandTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ShowAllCardsTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ToStringTest()
+        {
+            Assert.Fail();
+        }
+
+        
+    }
+}
