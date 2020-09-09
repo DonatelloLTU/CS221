8e4d4ca4a3bbb0207aaae41875c55d380cab9882
DonatelloLTU <60147196+DonatelloLTU@users.noreply.github.com>
2020-09-08 23:19:03 -0400

    Added Card ADT

---
+++ PlayingCards/src/Card.java
@@ -0,0 +1,34 @@
+import java.awt.*;
+import java.awt.image.*;
+import javax.swing.*;
+import java.net.URL;
+import java.io.File;
+import java.nio.file.Paths;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+/**
+ * Write a description of class Card here.
+ *
+ * @author Donatas Vasauskas
+ * @version 2020.09.06-01
+ */
+public class Card {
+
+    int suit;
+    int value;
+    private static String[] suits = {"Hearts", "Clubs", "Spades", "Diamonds"};
+    private static String[] values = {"2", "3", "4", "5", "6", "7", "8", "9", "10",
+            "J", "Q", "K", "A"};
+    boolean visible;
+
+    /**
+     * Constructor for objects of class Card
+     */
+    public Card(int suit, int value, File png) {
+        this.suit = suit;
+        this.value = value;
+        visible = true;
+
+    }
+
+}
---
+++ PlayingCards/src/CardADT.java
@@ -0,0 +1,45 @@
+import java.io.File;
+
+public class CardADT {
+    int suite;
+    int value;
+    int rank;
+    File graphics;
+
+    public CardADT ()
+    {
+
+    }
+    public void show()
+    {
+
+    }
+    public void setValue()
+    {
+
+    }
+    public int getValue()
+    {
+
+    }
+    public void setRank()
+    {
+
+    }
+    public int getRank()
+    {
+
+    }
+    public void setSuit()
+    {
+
+    }
+    public int getSuit()
+    {
+
+    }
+    public String toString()
+    {
+
+    }
+}
