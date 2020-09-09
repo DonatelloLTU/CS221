2e48b68bf49323f33007d055017e3f613195269d
DonatelloLTU <60147196+DonatelloLTU@users.noreply.github.com>
2020-09-08 23:36:33 -0400

    Tests

--- PlayingCards/src/CardADTTest.java
+++ PlayingCards/src/CardADTTest.java
@@ -1,2 +1,49 @@
-public class CardADTTest {
+import org.junit.Test;
+import static org.junit.Assert.*;
+import java.util.Arrays;
+import java.util.Random;
+
+public class CardADTTest extends CardADT
+{
+
+    CardADT cardADT = new CardADT();
+
+    @Test
+    public void setValueTest()
+    {
+
+
+    }
+    @Test
+    public void getValueTest()
+    {
+
+    }
+    @Test
+    public void setRankTest()
+    {
+
+    }
+    @Test
+    public void getRankTest()
+    {
+
+    }
+    @Test
+    public void setSuitTest()
+    {
+
+    }
+    @Test
+    public void getSuitTest()
+    {
+
+    }
+    @Test
+    public void toStringTest()
+    {
+
+    }
+
+
 }
