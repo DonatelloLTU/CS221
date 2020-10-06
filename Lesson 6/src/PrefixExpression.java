3474c97ee7db0f6dc083cd689fa791a3a91a59a0
DonatelloLTU <60147196+DonatelloLTU@users.noreply.github.com>
2020-10-06 16:11:59 -0400

    Added PrefixExpression class

---
+++ Lesson 6/src/PrefixExpression.java
@@ -0,0 +1,32 @@
+/**
+ * PrefixExpression class implements PrefixExpressionADT
+ * @author Donatas Vasauskas
+ * @version 2020-10-06
+ */
+public class PrefixExpression implements PrefixExpressionADT{
+
+    /**
+     * Constructor for the class
+     */
+    public PrefixExpression()
+    {
+
+    }
+    /**
+     * isPrefix method to check if the value PrefixExpression or not
+     * @return true or false boolean depending on the expression
+     */
+    public boolean isPrefix()
+    {
+        return true;
+    }
+
+    /**
+     * calculate method uses calculations to calculate the prefixExpression
+     * @return double number which is value after calculations
+     */
+    public double calculate()
+    {
+     return -1;
+    }
+}
